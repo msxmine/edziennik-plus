3ac5334e142a990f208aef95d6e6d6c6bb916977
msxmine <michal.mis.wroc@gmail.com>
2018-03-21 21:17:34 +0000

    0.3.5
    Fix X-Frame-Options header injection for Firefox 59

--- tlo.js
+++ tlo.js
@@ -1,15 +1,15 @@
 
-function zmienNaglowek(req) {
-    var mojNaglowek = {
-        name: "X-Frame-Options",
-        value: "SAMEORIGIN"
-    };
-    req.responseHeaders.push(mojNaglowek);
+function zmienNaglowek(req) {    
+    for (var i = 0, leng = req.responseHeaders.length; i < leng; i++){
+        if (req.responseHeaders[i].name == "X-Frame-Options"){
+            req.responseHeaders[i].value = "SAMEORIGIN";
+        }
+    }
     return {responseHeaders: req.responseHeaders};
 }
 
 browser.webRequest.onHeadersReceived.addListener(
     zmienNaglowek,
-    {urls: ["https://synergia.librus.pl/przegladaj_plan_lekcji"]},
+    {urls: ["https://synergia.librus.pl/przegladaj_plan_lekcji", "http://synergia.librus.pl/przegladaj_plan_lekcji"]},
     ["blocking", "responseHeaders"]
 );
